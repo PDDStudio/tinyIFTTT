27456d03108b42c757d9df6a236c4cbadef3f3b3
Patrick J <patrick.pddstudio@googlemail.com>
2016-03-29 23:06:23 +0200

    Added logic to ServerConnection, implemented Callback in MainActivity, added SendActionRequest skeleton

--- app/src/main/java/com/pddstudio/tinyiftt/MainActivity.java
+++ app/src/main/java/com/pddstudio/tinyiftt/MainActivity.java
@@ -1,13 +1,39 @@
 package com.pddstudio.tinyiftt;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.pddstudio.tinyiftt.connection.ServerConnection;
+import com.pddstudio.tinyiftt.models.TinyAction;
+
+public class MainActivity extends AppCompatActivity implements ServerConnection.ConnectionCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    @Override
+    public void onConnectingStarted() {
+
+    }
+
+    @Override
+    public void onConnectingFailed(@Nullable Throwable throwable) {
+
+    }
+
+    @Override
+    public void onConnectingFinished() {
+
+    }
+
+    @Override
+    public void onTinyActionFound(TinyAction tinyAction) {
+
+    }
+
 }
