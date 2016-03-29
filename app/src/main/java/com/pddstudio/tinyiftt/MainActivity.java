a43117feb50be98996f4c245e63d28c379360318
Patrick J <patrick.pddstudio@googlemail.com>
2016-03-29 23:48:33 +0200

    Added RecyclerView and Items for first UI test

--- app/src/main/java/com/pddstudio/tinyiftt/MainActivity.java
+++ app/src/main/java/com/pddstudio/tinyiftt/MainActivity.java
@@ -3,19 +3,32 @@ package com.pddstudio.tinyiftt;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.mikepenz.fastadapter.FastAdapter;
+import com.mikepenz.fastadapter.IAdapter;
+import com.mikepenz.fastadapter.IItem;
+import com.mikepenz.fastadapter.adapters.FastItemAdapter;
+import com.pddstudio.tinyiftt.adapter.TinyActionItem;
+import com.pddstudio.tinyiftt.connection.SendActionRequest;
 import com.pddstudio.tinyiftt.connection.ServerConnection;
 import com.pddstudio.tinyiftt.models.TinyAction;
 
-public class MainActivity extends AppCompatActivity implements ServerConnection.ConnectionCallback, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements ServerConnection.ConnectionCallback,
+        View.OnClickListener, FastAdapter.OnClickListener, SendActionRequest.Callback {
 
     private EditText ipAddressEditText;
     private EditText portEditText;
     private Button connectButton;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private FastItemAdapter<TinyActionItem> fastAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +39,13 @@ public class MainActivity extends AppCompatActivity implements ServerConnection.
         portEditText = (EditText) findViewById(R.id.portEditText);
         connectButton = (Button) findViewById(R.id.connectButton);
         if(connectButton != null) connectButton.setOnClickListener(this);
+        recyclerView = (RecyclerView) findViewById(R.id.itemRecyclerView);
+        layoutManager = new LinearLayoutManager(this);
+        fastAdapter = new FastItemAdapter<>();
+        fastAdapter.withOnClickListener(this);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(fastAdapter);
     }
 
 
@@ -48,6 +68,7 @@ public class MainActivity extends AppCompatActivity implements ServerConnection.
     @Override
     public void onTinyActionFound(TinyAction tinyAction) {
         Log.d("MainActivity", "TinyAction received: " + tinyAction.getActionIdentifier() + " Title: " + tinyAction.getActionTitle() + " Description: " + tinyAction.getActionDescription());
+        fastAdapter.add(new TinyActionItem(tinyAction));
     }
 
     @Override
@@ -59,4 +80,16 @@ public class MainActivity extends AppCompatActivity implements ServerConnection.
         serverConnection.execute();
     }
 
+    @Override
+    public boolean onClick(View v, IAdapter adapter, IItem item, int position) {
+        String serverIp = ipAddressEditText.getText().toString();
+        int serverPort = Integer.parseInt(portEditText.getText().toString());
+        new SendActionRequest(serverIp, serverPort, fastAdapter.getAdapterItem(position).getTinyAction()).sendRequest(this);
+        return true;
+    }
+
+    @Override
+    public void onActionSend(boolean success) {
+        Log.d("MainActivity", "Action Send : " + success);
+    }
 }
---
+++ app/src/main/java/com/pddstudio/tinyiftt/adapter/TinyActionItem.java
@@ -0,0 +1,57 @@
+package com.pddstudio.tinyiftt.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.mikepenz.fastadapter.items.AbstractItem;
+import com.pddstudio.tinyiftt.R;
+import com.pddstudio.tinyiftt.models.TinyAction;
+
+/**
+ * This Class was created by Patrick J
+ * on 29.03.16. For more Details and Licensing
+ * have a look at the README.md
+ */
+public class TinyActionItem extends AbstractItem<TinyActionItem, TinyActionItem.ViewHolder> {
+
+    private final TinyAction mTinyAction;
+
+    public TinyActionItem(TinyAction tinyAction) {
+        this.mTinyAction = tinyAction;
+    }
+
+    public TinyAction getTinyAction() {
+        return mTinyAction;
+    }
+
+    @Override
+    public int getType() {
+        return R.id.tiny_action_item;
+    }
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.item_tiny_action;
+    }
+
+    @Override
+    public void bindView(ViewHolder viewHolder) {
+        super.bindView(viewHolder);
+        viewHolder.actionTitle.setText(mTinyAction.getActionTitle());
+        viewHolder.actionDescription.setText(mTinyAction.getActionDescription());
+    }
+
+    protected static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView actionTitle;
+        TextView actionDescription;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.actionTitle = (TextView) itemView.findViewById(R.id.actionName);
+            this.actionDescription = (TextView) itemView.findViewById(R.id.actionDescription);
+        }
+
+    }
+}
